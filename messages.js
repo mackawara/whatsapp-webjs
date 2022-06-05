c550eaf8dbfee67f6a99de1b4857d033e1537ba9
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-05 09:17:58 +0200

    added a separate stroe for instant replies

---
+++ messages.js
@@ -0,0 +1,17 @@
+const messages = [
+  {
+    deliveryOptions: {
+      localSameday: "1.Local (in and around Harare)same day delivery",
+      localOvernight:
+        "2.Local : Delivered on the next day collection before 1600hrs",
+    },
+    parcelOptions: {
+      envelope: "Envelope",
+      cash: "Cash",
+      fastFoodBox: "Fast food Box",
+      groceryPack: "Groceries",
+    },
+  },
+];
+
+module.exports = messages;
