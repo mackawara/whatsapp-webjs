8ed09b7b7324be8820cf12815263871805ecde3d
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-05 23:56:02 +0200

    added bolierplate for chatbot

--- middleware/chatbot.js
+++ middleware/chatbot.js
@@ -1,9 +1,16 @@
-const messages= require("../messages");
-async function chatBot(client,request,customerNumber){
-const body=request.body
-client.sendMessage(customerNumber,fromToOption)
-customerNumber.addEventListener("")
+const restify = require('restify');
+const botbuilder = require('botbuilder');
 
-}
+// Create bot adapter, which defines how the bot sends and receives messages.
+var adapter = new botbuilder.BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
 
+// Create HTTP server.
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+});
 module.exports=chatBot
