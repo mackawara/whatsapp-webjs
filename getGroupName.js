7a13d11451439a0e522c32baf7c8d422eee6620c
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-06 22:27:16 +0200

    removed experimental code

--- getGroupName.js
+++
@@ -1,7 +0,0 @@
-async function getGroupName(expression,stringToMatch,groups){
-const regexExpression=`/${expression}/`
-groups.array.forEach(group => {
-    if (expression.match(regexExpression)){ return group} else return expression 
-});
-
-}
