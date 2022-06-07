293311c6c78baed5dd009c9fb24843e9f264ad91
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-07 21:36:09 +0200

    svaed contacts in separae file

---
+++ contacts.js
@@ -0,0 +1,23 @@
+
+const hwgeCheapGadgets2 = process.env.HWANGECHEAPGADGETS2;
+const hwangeBusinessMarketing1 = process.env.HWANGEBUSINESSMARKETINGGROUP1;
+const hwangeBusinessMarketing2 = process.env.HWANGEBUSINESSMARKETINGGROUP2;
+const matNorthBusinessGroup = process.env.MATNORTHBUSINESSGROUP;
+const hwangeCityTraders = process.env.HWANGECITYTRADERS;
+const hwangeBuyingAndSelling = process.env.HWANGEBUYINGANDSELLING;
+const hwangeClassifieds = process.env.HWANGECLASSIFIEDS;
+const hwangeDealsGrp1 = process.env.HWANGEDEALSGRP1;
+const amnestyinternational = process.env.AMNESTYINTERNATIONAL;
+
+
+const contactListForAds = [
+    hwangeBusinessMarketing1,
+    hwangeBusinessMarketing2,
+    matNorthBusinessGroup,
+    hwangeBuyingAndSelling,
+    hwangeCityTraders,
+    hwangeClassifieds,
+    hwangeDealsGrp1,
+    hwgeCheapGadgets2
+  ]
+  module.exports=contactList;
