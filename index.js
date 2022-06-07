8f10d3785f6b06e07b5d8971d8dee787ce0b2329
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-07 17:32:47 +0200

    added new advert mesasages

--- index.js
+++ index.js
@@ -35,8 +35,9 @@ const contactListForAds = [
 //Messages
 
 const advertMessages = [
-  " Contact *Millennium Printers* for all major brands of printer cartridges, HP , Kyocera, Nashua, Lexmark, Canon , Samsung i.e. All Toner,ink cartridges, master & ink for copiers are available. Call or whatsapp *0775231426* or visit our shop at Total Baobab Service Station",
-  " Genuine printer consumables available at great prices. Visit Millennium Printers, Shop 1 Baobab Service Station. In stock Hp ,Kyocera,Nashua, Ricoh,Samsung and all other major brands, Call us or whatsapp on 0775 231 426 for enquiries,or visit our shop at Total Baobab Service Station",
+  " Contact *Millennium Printers* for all major brands of printer cartridges, HP , Kyocera, Nashua, Lexmark, Canon , Samsung i.e. All Toner,ink cartridges, master & ink for copiers are available. \n Call or whatsapp *0775231426* or visit our shop at Total Baobab Service Station",
+  ` Genuine printer consumables available at great prices. Visit Millennium Printers, Shop 1 Baobab Service Station.In stock Hp ,Kyocera,Nashua, Ricoh,Samsung and all other major brands \n Call us or whatsapp on 0775 231 426 for enquiries,or visit our shop at Total Baobab Service Station \n 
+  * New shop Opening at Pick Pay malll*`,
   "*Millennium Printers*.The premier suppliers of all IT products . Computers,printers, IT accessories and spare parts, toner powder, toner cartridge replacement parts etc.  Call or whatsapp *0775231426* or visit our shop at Total Baobab Service Station",
   `*CCTV Installation* \n Secure your home or business premises with High quality IP Surveillance Cameras \n
    *Remote Monitoring* \n monitor live feed from all your cameras from your phone/computer from anywhere in the world \n
@@ -53,7 +54,25 @@ const advertMessages = [
     - PtP wireless links \n
     Contact us on *0775231426* or visit our shop at Total Baobab Service Station`,
   ` Reliable,Proffessional Computer hardware and Software Repairs by industry experts. \n Please inbox for enquiries`,
-  `HP 415 Ink tank Wireless \n Ships with 15000 pages worth of ink, You wont have to buy ink for almost 2 years`,
+  `HP 415 Ink tank Wireless \n Ships with 15000 pages worth of ink, You wont have to buy ink for almost 2 years. 
+  \n Priced at only $260 it is ideal for small office or home use. For enquiries , please call us on *0775231426* or 0812830008 (landline)`,
+  `Opening Soon at TM Pickn PAy mall  (Shops directly above TM pick N Pay). 
+    *Venta Tech * \n A revolutionary IT Shop  run by technology legends \n
+     *Services on Offer * \n
+     1. Web Design / Web app development \n
+     2. Professional ,competent reliable *Computer repairs* \n
+     3. CCTV Installation \n
+     4. Network Installation \n
+     5. Satelite Dish Installation , (Satelite TV / Eutel SAT internet) \n
+     6. *Bulk* printing/Photocopying \n
+     
+     *PRODUCTS * \n
+     1. PRINTER CONSUMABLES ( Cartridges , inks, toner powder) \n
+     2. PRINTERS \n
+     3. Stationery \n
+     4. Laptops / Desktops\n
+     For enquiries vist us at our current shop at Baobab S/ Station , call /whatsapp 0775231426
+     `,
 ];
 
 let randomAdvert = () =>
@@ -122,7 +141,7 @@ async function sendAdverts() {
 client.on("ready", () => {
   console.log("Client is ready!");
   cron.schedule(
-    "30 7,14 * * *",
+    "47 11,14 * * *",
     () => {
       sendAdverts();
     },
