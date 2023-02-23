c67f902fe20548c89d45e5861c89f731ddd68d56
mackawara <mkawara@outlook.com>
2023-02-23 10:37:25 +0200

    feat set headless to true:

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const SESSION_FILE_PATH = "./session.json";
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    headless: false,
+    headless: true,
     //"--no-sandbox": true,
     "--disable-setuid-sandbox": true,
     executablePath:
