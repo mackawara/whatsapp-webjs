9fcc8f9906d27710f01a02414fd5d4ab922850d3
mackawara <mkawara@outlook.com>
2023-03-02 17:04:20 +0200

    checking i f string

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dbString=process.env.DB_STRING
 
 const connectDB = async () => {
   try {
+    console.log(dbString)
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
