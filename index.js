5754502040446aeb72503f15b54ab194a678277d
mackawara <mkawara@outlook.com>
2023-04-21 07:29:10 +0200

    refact: changed scope

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
-require("dotenv").config();
-
-const keywordAlert = require("./keywordsAlert");
-
-//Helper Functions
-
-const readFile = require("./config/helperFunction/readFile");
-const timeDelay = (ms) => new Promise((res) => setTimeout(res, ms));
-const cron = require("node-cron");
-const uploadImage = require("./middleware/uploadImage");
-const cloudinary = require("./middleware/cloudinary");
-
-const todayDate = new Date().toISOString().slice(0, 10);
-//Db models
-const matchIDModel = require("./models/cricketMatchIds");
-const contactModel = require("./models/contactsModel");
-
-// APi calls functions
-//football API
-const callFootballApi = require("./config/helperFunction/callFootballApi");
-const getCommentary = require("./config/getCommentary");
-
-//CIRCKET SCORES
-const getMatchIds = require("./config/getMatchIds");
-
-//database quieries
-const getFixtures = require("./config/helperFunction/getFixtures");
-// connect to mongodb before running anything on the app
-
 const connectDB = require("./config/database");
-const { Client, LocalAuth, MessageMedia, id } = require("whatsapp-web.js");
-const mongoose = require("mongoose");
-const { schedule } = require("node-cron");
-const { exit } = require("process");
 
-//contacts
-const tate = process.env.TATENDA;
-const me = process.env.ME;
-//groups
-const hwangeClubCricket = process.env.HWANGECLUBDELACRICKET;
-const liveSoccer1 = process.env.LIVESOCCER1;
-const liveCricket1 = process.env.LIVECRICKET1;
-const hwangeDealsgrp1 = "263775932942-1555492418@g.us";
-const sellItHge4 = "263773389927-1588234038@g.us";
-const sellIthge = "263717766191-1583426999@g.us";
-const sellIthge2 = "263717766191-1583474819@g.us";
-const hwangeclassifieds = "263714496540-1579592614@g.us";
-const hwangeCitytraders = "263774750143-1590396559@g.us";
-
+require("dotenv").config();
+// connect to mongodb before running anything on the app
 connectDB().then(async () => {
+  const { Client, LocalAuth } = require("whatsapp-web.js");
+
   const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-      executablePath: "/usr/bin/chromium-browser",
+      // executablePath: "/usr/bin/chromium-browser",
       handleSIGINT: true,
       headless: true,
       args: [
@@ -67,48 +24,100 @@ connectDB().then(async () => {
 
   //messaging client resources
   const clientOn = require("./config/helperFunction/clientOn");
-  clientOn(client, `message`);
-  clientOn(client, "group-join");
-  clientOn(client, "group-leave");
+
+  //client
   clientOn(client, "authenticated");
   clientOn(client, "auth_failure");
   clientOn(client, "qr");
-
-  let randomAdvert = () =>
-    advertMessages[Math.floor(Math.random() * advertMessages.length)];
-
-  let advertMessages = require("./adverts");
-  //client
-
-  const contactListForAds = [
-    hwangeDealsgrp1,
-    sellIthge,
-    sellIthge2,
-    sellItHge4,
-    hwangeCitytraders,
-    hwangeclassifieds,
-  ];
-  async function sendAdverts() {
-    const contact = "263775231426@c.us"; //contactListForAds[index];
-    for (let i = 0; i < contactListForAds.length; i++) {
-      try {
-        client
-          .sendMessage(contactListForAds[i], `${randomAdvert()}`)
-          .catch((error) => {
-            console.log(error);
-          });
-        await timeDelay(5000);
-      } catch (error) {
-        console.log(error);
-        client.sendMessage(
-          me,
-          `failed to send automatic message to ${contactListForAds[i]}`
-        );
-      }
-    }
-  }
-
   client.on("ready", async () => {
+    //functions abd resources
+    //Helper Functions
+    const getHrsMins = require("./config/helperFunction/getHrsMins");
+    const timeConverter = require("./config/helperFunction/timeConverter");
+    const cron = require("node-cron");
+
+    //Db models
+    const matchIDModel = require("./models/cricketMatchIds");
+
+    // APi calls functions
+    //football API
+    const callFootballApi = require("./config/helperFunction/callFootballApi");
+    const getCommentary = require("./config/getCommentary");
+
+    //CIRCKET SCORES
+    const getMatchIds = require("./config/getMatchIds");
+
+    //database quieries
+    const getFixtures = require("./config/helperFunction/getFixtures");
+
+    clientOn(client, `message`);
+    clientOn(client, "group-join");
+    clientOn(client, "group-leave");
+
+    //decalre variables that work with client here
+
+    //contacts
+    const me = process.env.ME;
+    //groups
+    const hwangeClubCricket = process.env.HWANGECLUBDELACRICKET;
+    const liveSoccer1 = process.env.LIVESOCCER1;
+    const liveCricket1 = process.env.LIVECRICKET1;
+    const hwangeDealsgrp1 = "263775932942-1555492418@g.us";
+    const sellItHge4 = "263773389927-1588234038@g.us";
+    const sellIthge = "263717766191-1583426999@g.us";
+    const sellIthge2 = "263717766191-1583474819@g.us";
+    const sellIthge5 = "263717766191-1611592932@g.us";
+    const sellIthge3 = "263717766191-1584895535@g.us";
+    const sellIthge6 = "263717766191-1616870613@g.us";
+    const hwangeclassifieds = "263714496540-1579592614@g.us";
+    const hwangeCitytraders = "263774750143-1590396559@g.us";
+    const noCaptBusIdeas = "263783046858-1621225929@g.us";
+
+    const contactListForAds = [
+      hwangeDealsgrp1,
+      sellIthge,
+      sellIthge2,
+      sellItHge4,
+      hwangeCitytraders,
+      hwangeclassifieds,
+      sellIthge5,
+      sellIthge6,
+      sellIthge3,
+      noCaptBusIdeas,
+    ];
+
+    client.on(`message`, async (message) => {
+      const keywords = require("./keywords");
+      const usdKeywords = keywords.usdKeyword;
+      const businessKeywords = keywords.businessKeywords;
+
+      usdKeywords.filter((keyword) => {
+        if (
+          message.body.includes(keyword) &&
+          !message.body.includes(`message created by chatBot`)
+        ) {
+          client.sendMessage(
+            me,
+            `${timeConverter(message.timestamp)[0]}  at ${
+              message.from
+            } group :message from :${message.author
+              .replace("@c.us", "")
+              .replace("263", "0")}, ${message.notifyName} ${
+              message.body
+            } *message created by chatBot*`
+          );
+        }
+      });
+
+      const KeywordAlert = require("./keywordsAlert");
+      let businessAlert = new KeywordAlert(
+        businessKeywords,
+        client,
+        message,
+        me
+      );
+      businessAlert.keywordRun(message.body);
+    });
     client.setDisplayName("Live Scores,news, articles");
     let daysMatchIDs = [];
     let matchIdMessage = [];
@@ -121,8 +130,7 @@ connectDB().then(async () => {
         console.log("cricket running");
         await getMatchIds("upcoming", "League");
         await getMatchIds("upcoming", "International");
-        // await getMatchIds("upcoming", "Domestic");
-        let startTimes = [];
+
         //find the day`s cricket matchs and save their match Ids to the DB
         const cricketMatchesToday = await matchIDModel
           .find({
@@ -239,6 +247,28 @@ connectDB().then(async () => {
       },
       { scheduled: true, timezone: `Africa/Harare` }
     );
+    let randomAdvert = () =>
+      advertMessages[Math.floor(Math.random() * advertMessages.length)];
+
+    let advertMessages = require("./adverts");
+    async function sendAdverts() {
+      for (let i = 0; i < contactListForAds.length; i++) {
+        try {
+          client
+            .sendMessage(contactListForAds[i], `${randomAdvert()}`)
+            .catch((error) => {
+              console.log(error);
+            });
+          await timeDelay(5000);
+        } catch (error) {
+          console.log(error);
+          client.sendMessage(
+            me,
+            `failed to send automatic message to ${contactListForAds[i]}`
+          );
+        }
+      }
+    }
     cron.schedule(`15 9,11,13,14,16,18 * * *`, () => sendAdverts(), {
       scheduled: true,
       timezone: `Africa/Harare`,
@@ -249,67 +279,6 @@ connectDB().then(async () => {
     //});
 
     console.log("Client is ready!");
-  });
-
-  // client.sendMessage(me,uclFixtures)
-  function toTime(UNIX_timestamp) {
-    const a = new Date(UNIX_timestamp * 1000);
-    const months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-    const year = a.getFullYear();
-    const month = months[a.getMonth()];
-    const date = a.getDate();
-    const hour = a.getHours();
-    const min = a.getMinutes();
-    const sec = a.getSeconds();
-    const time =
-      date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
-    return time;
-  }
-
-  const messages = require("./messages");
-  //const getmatch = await getCommentary("66173");
-
-  client.on(`message`, async (message) => {
-    const messageContents = message.body;
-    const author = message.from.replace("@c.us", "");
-    const receiver = message.to.replace("@c.us", "").replace("263", "0");
-    const keywords = require("./keywords");
-    const usdKeywords = keywords.usdKeyword;
-    const businessKeywords = keywords.businessKeywords;
-
-    usdKeywords.filter((keyword) => {
-      if (
-        message.body.includes(keyword) &&
-        !message.body.includes(`message created by chatBot`)
-      ) {
-        client.sendMessage(
-          me,
-          `${toTime(message.timestamp)}  at ${
-            message.from
-          } group :message from :${message.author
-            .replace("@c.us", "")
-            .replace("263", "0")}, ${message.notifyName} ${
-            message.body
-          } *message created by chatBot*`
-        );
-      }
-    });
-
-    let businessAlert = new keywordAlert(businessKeywords, client, message, me);
-    businessAlert.keywordRun(message.body);
   });
 
   client.on("disconnected", (reason) => {
