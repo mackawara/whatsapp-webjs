dc99959b1283e267748001ed9381b12b7aa9c250
mackawara <mkawara@outlook.com>
2023-04-25 15:27:06 +0200

    feat: fetches news headlines

---
+++ config/helperFunction/getCricketHeadlines.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const writeFile = require("writeFile");
+const getCricketHeadlines = async () => {
+  console.log("get cricket headlines running");
+  const options = {
+    method: "GET",
+
+    url: "https://cricbuzz-cricket.p.rapidapi.com/news/v1/detail/122025",
+    headers: {
+      "content-type": "application/octet-stream",
+      "X-RapidAPI-Key": process.env.RAPIDAPIKEY,
+      "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    writeFile(response.data, "headlines.json");
+  } catch (error) {
+    console.error(error);
+  }
+};
+module.exports = getCricketHeadlines;
