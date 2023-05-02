d3c87d8854767fbcd81680a50ed5107f0896f8ff
mackawara <mkawara@outlook.com>
2023-05-02 07:37:43 +0200

    :dev

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const connectDB = require("./config/database");
 
-const getCommentary = require("./config/helperFunction/getCricComm");
 
 require("dotenv").config();
 // connect to mongodb before running anything on the app
@@ -10,7 +9,7 @@ connectDB().then(async () => {
   const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-      executablePath: "/usr/bin/chromium-browser",
+      //executablePath: "/usr/bin/chromium-browser",
       handleSIGINT: true,
       headless: true,
       args: [
@@ -59,6 +58,85 @@ connectDB().then(async () => {
     //decalre variables that work with client here
 
     client.setDisplayName("Live Scores,news, articles");
+
+    const liveCricket1 = "120363110873098533@g.us";
+    const getMatchIds = require("./config/helperFunction/getMatchIds");
+    const getCommentary = require("./config/helperFunction/getCricComm");
+
+    // get the latest updates
+
+    let calls = 0;
+    const date = new Date(),
+      yestdate = date.setDate(date.getDate() - 1);
+    cron.schedule("15 3 * * *", async () => {
+      //await getMatchIds("upcoming");
+      await getMatchIds("recent", calls);
+      const completedMatches = await matchIDModel.find({
+        date: new Date(yestdate).toISOString().slice(0, 10),
+        matchState: /complete/gi,
+      });
+      console.log(completedMatches);
+      completedMatches.forEach(async (match) => {
+        const commentary = await getCommentary(match.matchID, calls);
+        client.sendMessage(liveCricket1, commentary);
+      });
+      console.log(upcoming);
+      const upcoming = await matchIDModel({
+        date: new Date().toISOString().slice(0, 10),
+        matchState: /upcoming|preview/gi,
+      });
+      upcoming.forEach(async (match) => {
+        const commentary = await getCommentary(match.matchID, calls);
+        client.sendMessage(liveCricket1, commentary);
+      });
+    });
+    const timeDelay = (ms) => new Promise((res) => setTimeout(res, ms));
+    //find the day`s cricket matchs and save their match Ids to the DB
+    console.log(new Date().toISOString().slice(0, 10));
+    cron.schedule(`30 3 * * *`, async () => {
+      await getMatchIds("upcoming", calls);
+
+      //at 215am everyday get the international and Ipl matches for the day and put them in an array
+      await matchIDModel
+        .find({
+          date: new Date().toISOString().slice(0, 10),
+          matchState: /upcoming/gi,
+          //matchType: /league|ODI|test|T20i/gi,
+        })
+        .exec()
+        .then((matchesToday) => {
+          console.log(matchesToday);
+          matchesToday.forEach(async (match) => {
+            console.log("upcoming matches");
+            // const commentary = await getCommentary(match.matchID);
+            //   client.sendMessage(`263775231426@c.us`, commentary);
+            // console.log(/upcoming/gi.test(match.matchState));
+            const hours = new Date(parseInt(match.unixTimeStamp)).getHours(),
+              minutes = new Date(parseInt(match.unixTimeStamp)).getMinutes();
+            console.log(hours);
+            // send live update for each game every 25 minutes
+            cron.schedule(`${minutes} ${hours} * * *`, async () => {});
+            //run at least once
+            do {
+             
+              //send message prefixed with group invite
+              const cricketGroupInvite = `https://chat.whatsapp.com/EW1w0nBNXNOBV9RXoize12`;
+              const commentary = await getCommentary(match.matchID, calls);
+              const message = [cricketGroupInvite, commentary];
+              client.sendMessage(liveCricket1, message.join("\n"));
+              calls > 85
+                ? client.sendMessage("263775231426", "calls going hig")
+                : console.log("waiting");
+              await timeDelay(1500000);
+            } while (
+              !/Complete/gi.test(await getCommentary(match.matchID, calls))
+            ); //if comms test returns true
+          });
+        });
+
+      // loop through the matches and get commentary every 15 minutes
+    });
+
     let mediaAdverts = [];
     const mediaModel = require("./models/media");
     client.on("message", async (msg) => {
