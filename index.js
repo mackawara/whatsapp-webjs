f41a5d211c759bc4a7948b5e56b06831b9a656be
mackawara <mkawara@outlook.com>
2023-05-09 16:29:46 +0200

    changed conditional

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ connectDB().then(async () => {
               `263775231426@c.us`,
               `match ${match.fixture} scheduled to run at ${hours}:${minutes}`
             );
-            cron.schedule(`30 ${hours} * * *`, async () => {
+            cron.schedule(`36 ${hours} * * *`, async () => {
               let commentary = await getCommentary(match.matchID, calls);
               if (!/not available/gi.test(commentary)) {
                 client.sendMessage(`${match.fixture} not available`);
