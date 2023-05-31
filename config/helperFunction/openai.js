1c291a649576275f9afa2054c86d34fd6ea353ed
mackawara <mkawara@outlook.com>
2023-05-31 18:34:29 +0200

    changed to gpt 3.5 turbo and change to chat completion

--- config/helperFunction/openai.js
+++ config/helperFunction/openai.js
@@ -1,6 +1,20 @@
 //openai
-const openAiCall = async (prompt) => {
+
+let chats = {};
+const openAiCall = async (prompt, chatID) => {
+  console.log(chats);
+  if (!chats[chatID]) {
+    Object.assign(chats, {
+      [chatID]: {
+        messages: [{ role: "user", content: prompt }],
+      },
+    });
+    console.log(chats);
+  } else {
+    chats[chatID].messages.push({ role: "user", content: prompt });
+  }
   console.log("openai called");
+  console.log(chats[chatID]["messages"]);
   const { Configuration, OpenAIApi } = require("openai");
   const configuration = new Configuration({
     organization: process.env.OPENAI_ORGANISATION_KEY,
@@ -8,16 +22,29 @@ const openAiCall = async (prompt) => {
   });
 
   const openai = new OpenAIApi(configuration);
+  let error;
   const response = await openai
-    .createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 0,
-      max_tokens: 1000,
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: chats[chatID]["messages"],
+      temperature: 0.3,
+      max_tokens: 150,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
     })
     .catch((err) => {
-      console.log(err);
+      console.log("Error recorded " + err.response.data.error);
+      error = err.response.data.error;
     });
-  return response.data.choices;
+  if (response.data) {
+    console.log("no errors");
+    console.log(response.data.choices[0]["message"]);
+    console.log(chats[chatID]["messages"]);
+    chats[chatID].messages.push(response.data.choices[0]["message"]);
+    console.log(chats);
+    return response.data.choices[0]["message"]["content"];
+  } else {
+    return error.message;
+  }
 };
 module.exports = openAiCall;
