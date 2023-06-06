d57b3192cbda8a912396d3093edec40f65f5b285
mackawara <mkawara@outlook.com>
2023-06-06 17:07:35 +0200

    not working yet but ogic for blocking micreants

--- config/helperFunction/openai.js
+++ config/helperFunction/openai.js
@@ -12,7 +12,7 @@ const openAiCall = async (prompt, chatID) => {
     .find({ serialisedNumber: chatID })
     .exec();
   const contact = contacts[0];
-  
+
   //check if there is an existing chat from that number and create if not
 
   if (!chats[chatID]) {
@@ -20,11 +20,13 @@ const openAiCall = async (prompt, chatID) => {
     Object.assign(chats, {
       [chatID]: {
         messages: [{ role: "user", content: prompt }],
+        calls: 0,
       },
     });
     console.log(chats);
   } else {
     console.log("found existing chat");
+    chats[chatID].calls++;
     chats[chatID].messages.push({ role: "user", content: prompt });
   }
 
@@ -36,39 +38,55 @@ const openAiCall = async (prompt, chatID) => {
 
   const openai = new OpenAIApi(configuration);
   let error;
-  const response = await openai
-    .createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: chats[chatID]["messages"],
-      temperature: 0.3,
-      max_tokens: 250,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    })
-    .catch((err) => {
-      console.log("Error recorded " + err.response.data.error);
-      error = err.response;
-    });
-  console.log(response.data);
-  if ("data" in response) {
-    console.log("no errors");
-    console.log(response.data.choices[0]["message"]);
-    chats[chatID].messages.push(response.data.choices[0]["message"]); //add system response to messages
-    chats[chatID].messages.splice(0, chats[chatID].messages.length - 4); //trim messages and remain wit newest ones only
-    console.log(chats[chatID].messages.length);
-    setTimeout(() => {
-      chats[chatID]["calls"] = 0;
-    }, 1000);
-    setTimeout(() => {
-      chats[chatID]["message"] = [];
-    }, 180000); // messages are forgotten after 30mins
-    contact.tokens =
-      parseInt(contact.tokens) + response.data.usage.total_tokens;
-    contact.calls = parseInt(contact.calls) + 1;
-    contact.save();
-    return response.data.choices[0]["message"]["content"];
+  if (chats[chatID]["calls"] < 2) {
+    const response = await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: chats[chatID]["messages"],
+        temperature: 0.3,
+        max_tokens: 250,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      })
+      .catch((err) => {
+        console.log("Error recorded " + err.response.data.error.message);
+        console.log(err);
+        error = err.response;
+        return;
+      });
+
+    if ("data" in response) {
+      chats[chatID]["calls"]++; // add call count
+      chats[chatID].messages.push(response.data.choices[0]["message"]); //add system response to messages
+      chats[chatID].messages.splice(0, chats[chatID].messages.length - 4); //trim messages and remain wit newest ones only
+      console.log(chats[chatID].messages.length);
+      contact.calls = parseInt(contact.calls) + 1;
+      setTimeout(() => {
+        chats[chatID]["calls"] = 0;
+      }, 15000); // reset the calls in local store
+      setTimeout(() => {
+        chats[chatID]["message"] = [];
+      }, 180000); // messages are forgotten after 30mins
+      contact.tokens =
+        parseInt(contact.tokens) + response.data.usage.total_tokens;
+      contact.calls = parseInt(contact.calls) + 1;
+      contact.save();
+      return response.data.choices[0]["message"]["content"];
+    } else {
+      return error.message;
+    }
   } else {
-    return error.message;
+    contact.warnings = contact.warnings + 1;
+    if (contact.warnings > 10) {
+      contact.isBlocked = true;
+    }
+
+    try {
+      contact.save();
+    } catch (error) {
+      console.log(error);
+    }
+    return "*Error!* too many requests made , please try later. You cannot make mutiple requests at the same time";
   }
 };
 module.exports = openAiCall;
