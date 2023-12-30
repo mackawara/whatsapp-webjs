125e49fd51e0fd79110c2fa4dc89771880b32557
mackawara <mkawara@outlook.com>
2023-12-30 14:59:30 +0200

    feat: add test logic in controller

--- controllers/liveGames/liveGame.controller.js
+++ controllers/liveGames/liveGame.controller.js
@@ -5,40 +5,39 @@ const cron = require('node-cron');
 const client = require('../../config/wwebjsConfig');
 const system = require('../../constants/system');
 
-const scoresUpdate = fixturesToday => {
-  gamesRemainingToday = fixturesToday.map(fixture => fixture.timestamp);
-
+const scoresUpdate = async fixturesToday => {
+  let gamesRemainingToday = fixturesToday.map(fixture => fixture.timestamp);
+  console.log(gamesRemainingToday);
   let cronString = utils.generateCronScheduleForgames(gamesRemainingToday);
-
-  const liveUpdateJob = cron.schedule(cronString, async () => {
-    const liveScores = await getLiveScores(live); // if empty string it means no score
-    while (!liveScores == '' && !gamesRemainingToday.length == 0) {
-      console.log('cron triggerd');
-      if (liveScores === '') {
-        const completedMatchIds = gamesRemainingToday
-          .filter(fixture => isBefore(fixture.timestamp, new Date()))
-          .map(fixture => fixture.fixtureId);
-        const fulltimeScores = await getLiveScores(
-          completed,
-          completedMatchIds
-        );
-        sendUpdateToGroup(
-          system.AMNESTYGROUP,
-          `Live Updates every 10 minutes \n\n ${fulltimeScores}`
-        );
-      } else {
-        sendUpdateToGroup(
-          system.AMNESTYGROUP,
-          `Live Updates every 10 minutes \n\n ${liveScores}`
-        );
-        utils.timeDelay(system.UPDATE_INTERVAL);
-      }
-      gamesRemainingToday = gamesRemainingToday.filter(fixture =>
-        isAfter(fixture.timestamp, new Date())
-      ); // continously filters to see if any games are remaining that day
+  console.log(cronString);
+  // const liveUpdateJob = cron.schedule(cronString, async () => {
+  const liveScores = await getLiveScores(live); // if empty string it means no score
+  while (!liveScores == '' && !gamesRemainingToday.length == 0) {
+    console.log('cron triggerd');
+    if (liveScores === '') {
+      const completedMatchIds = gamesRemainingToday
+        .filter(fixture => isBefore(fixture.timestamp, new Date()))
+        .map(fixture => fixture.fixtureId);
+      const fulltimeScores = await getLiveScores(completed, completedMatchIds);
+      sendUpdateToGroup(
+        system.AMNESTYGROUP,
+        `Live Updates every 10 minutes \n\n ${fulltimeScores}`
+      );
+      console.log('no liv fixtures in progress');
+    } else {
+      sendUpdateToGroup(
+        system.AMNESTYGROUP,
+        `Live Updates every 10 minutes \n\n ${liveScores}`
+      );
+      utils.timeDelay(system.UPDATE_INTERVAL);
     }
-    liveUpdateJob.stop();
-  });
+    gamesRemainingToday = gamesRemainingToday.filter(fixture =>
+      isAfter(fixture.timestamp, new Date())
+    ); // continously filters to see if any games are remaining that day
+    console.log(gamesRemainingToday);
+  }
+  liveUpdateJob.stop();
+  // });
 };
 const sendUpdateToGroup = async (recipient, message) => {
   const maxDelayTimeInSecs = 97;
